2e6bc87634d100c33b1a139c7146cb0ac9ecb35f
Addisu Mekonene-Win <hanabob1994@gmail.com>
2025-06-08 14:28:21 +0300

    Task 3 -- Tempreture Conversion Tool

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -1,34 +1,3 @@
-"""# This is temp_conversion_tool.py
-# Define Global Conversion Factors:
-
-FAHRENHEIT_TO_CELSIUS_FACTOR = 5/9
-
-CELSIUS_TO_FAHRENHEIT_FACTOR = 9/5
-
-# Implement Conversion Functions:
-def convert_to_celsius(fahrenheit):
-    Celsius = FAHRENHEIT_TO_CELSIUS_FACTOR * (fahrenheit - 32)
-    return Celsius
-
-fahrenheit = int(input("Enter your tempreture in Celsius: "))
-#print(f"The Celsius equivalent for the fahrenheit is {fahrenheit}")
-convert_to_celsius(fahrenheit)
-
-def convert_to_fahrenheit(celsius):
-    Fahrenheit = CELSIUS_TO_FAHRENHEIT_FACTOR * celsius + 32
-    return Fahrenheit
-
-# User Interaction:
-input_tempreture = float(input("Enter the temperature to convert: "))
-celsius_or_fahrenheit = input("Is this temperature in Celsius or Fahrenheit? (C/F): ")
-
-if celsius_or_fahrenheit == 'F':
-    print(f"{input_tempreture}°F is {convert_to_celsius(float(input_tempreture))}°C")
-elif celsius_or_fahrenheit == 'C':
-    print(f"{input_tempreture}°C is {convert_to_fahrenheit(float(input_tempreture))}°F")
-else:
-    print("Please insert the exact number you want to convert!")    """
-
 # This is temp_conversion_tool.py
 """Celsius to Fahrenheit:
 F = 9/5 * C + 32
@@ -42,11 +11,11 @@ CELSIUS_TO_FAHRENHEIT_FACTOR = 9 / 5
 
 # Implement Conversion Functions:
 def convert_to_celsius(fahrenheit):
-    celsius = FAHRENHEIT_TO_CELSIUS_FACTOR * (fahrenheit - 32)
+    celsius = (fahrenheit - 32) * FAHRENHEIT_TO_CELSIUS_FACTOR
     return celsius
 
 def convert_to_fahrenheit(celsius):
-    fahrenheit = CELSIUS_TO_FAHRENHEIT_FACTOR * celsius + 32
+    fahrenheit = (celsius * CELSIUS_TO_FAHRENHEIT_FACTOR) + 32
     return fahrenheit
 
 # User Interaction with Input Validation:
